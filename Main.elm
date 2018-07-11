3a32c3914c55c587e6e18a545f11d99b72f68679
Robert Erb <rerb@mbp.local>
2018-07-11 17:56:32 -0400

    Upgrade to working simplest version (on branch `simplest`)
    Maybe this branch can be used as an example of doing HTML drag and
    drop in Elm.  Or maybe share at Elm Boston Meetup for feedback.

--- Main.elm
+++ Main.elm
@@ -1,19 +1,9 @@
 module Main exposing (main)
 
-import Bootstrap.Accordion as Accordion
-import Bootstrap.CDN as CDN
-import Bootstrap.Card as Card
-import Bootstrap.Card.Block as Block
-import Bootstrap.Grid as Grid
 import Debug exposing (crash, log)
 import DragAndDropEvents exposing (onDragStart, onDragOver, onDragEnd, onDrop)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-import Http exposing (..)
-import Issue exposing (Issue, decodeIssue, emptyIssue)
-import Post exposing (Post)
-import Section exposing (Section)
 
 
 main : Program Never Model Msg
@@ -22,7 +12,7 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = subscriptions
+        , subscriptions = (\m -> Sub.none)
         }
 
 
@@ -30,30 +20,41 @@ main =
 -- Model
 
 
+type alias Post =
+    { title : String
+    , position : Int
+    }
+
+
 type alias Model =
-    { issue : Issue
-    , status : String
-    , accordionState : Accordion.State
+    { posts : List Post
     , movingPost : Maybe Post
     , draggedOverPost : Maybe Post
     , droppedOnPost : Maybe Post
-    , sectionsToSave : List Section
-    , showSaveButton : Bool
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { issue = emptyIssue -- <-- smells to me
-      , status = ""
-      , accordionState = Accordion.initialState
+    ( { posts =
+            [ { title = "Post One"
+              , position = 1
+              }
+            , { title = "Post Two"
+              , position = 2
+              }
+            , { title = "Post Three"
+              , position = 3
+              }
+            , { title = "Post Four"
+              , position = 4
+              }
+            ]
       , movingPost = Nothing
       , draggedOverPost = Nothing
       , droppedOnPost = Nothing
-      , sectionsToSave = []
-      , showSaveButton = False
       }
-    , (getIssue 515)
+    , Cmd.none
     )
 
 
@@ -62,103 +63,21 @@ init =
 
 
 type Msg
-    = LoadIssue (Result Http.Error Issue)
-    | AccordionMsg Accordion.State
-    | DragStart Post
+    = DragStart Post
     | DragEnd
     | DropOn Post
     | DragOver Post
-    | SaveSectionOrder Section
-
-
-getSectionForPost : Model -> Post -> Section
-getSectionForPost model post =
-    let
-        sections =
-            List.filter (\s -> List.member post s.posts)
-                model.issue.sections
-    in
-        case List.head sections of
-            Nothing ->
-                Debug.crash "getSectionForPost: no section for post"
-
-            Just section ->
-                section
-
-
-removePostFromSection : Section -> Post -> Section
-removePostFromSection section post =
-    let
-        remainingPosts =
-            List.filter (\p -> p /= post) section.posts
-
-        postsSlidUp =
-            slidePostsUp remainingPosts post
-
-        _ =
-            Debug.log "remainingPosts" remainingPosts
-    in
-        { section | posts = postsSlidUp }
-
-
-slidePostsUp : List Post -> Post -> List Post
-slidePostsUp posts post =
-    List.map
-        (\p ->
-            if p.position >= post.position then
-                { p | position = p.position - 1 }
-            else
-                p
-        )
-        posts
-
-
-removePostFromIssue : Model -> Issue -> Post -> Issue
-removePostFromIssue model issue post =
-    let
-        sections =
-            List.map (\s -> removePostFromSection s post) issue.sections
-    in
-        { issue | sections = sections }
-
-
-insertPostInIssue : Issue -> Section -> Post -> Post -> Issue
-insertPostInIssue issue section insertThis insertBefore =
-    let
-        otherSections = List.filter (\s -> section /= s) issue.sections
-        targetSection = List.filter (\s -> section == s) issue.sections
-                      |> List.head
-        sections = otherSections ++
-                   [insertPostInSection section insertThis insertBefore]
-    in
-        { issue | sections = sections }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        LoadIssue (Ok issue) ->
-            ( { model | issue = issue }, Cmd.none )
-
-        LoadIssue (Err msg) ->
-            ( { model | status = toString msg }, Cmd.none )
-
-        AccordionMsg state ->
-            ( { model | accordionState = state }
+        DragStart post ->
+            ( { model
+                | movingPost = Just post
+              }
             , Cmd.none
             )
-
-        DragStart post ->
-            let
-                issue =
-                    removePostFromIssue model model.issue post
-            in
-                ( { model
-                    | movingPost = Just post
-                    , issue = issue
-                  }
-                , Cmd.none
-                )
 
         DragEnd ->
             ( { model
@@ -169,30 +88,11 @@ update msg model =
 
         DropOn post ->
             let
-                movingPost =
-                    case model.movingPost of
-                        Nothing ->
-                            Debug.crash "No model.movingPost"
-
-                        Just movingPost ->
-                            movingPost
-
-                section =
-                    getSectionForPost model post
-
-                issue =
-                    insertPostInIssue model.issue section movingPost post
-
-                sectionsToSave =
-                    if List.member section model.sectionsToSave then
-                        model.sectionsToSave
-                    else
-                        List.append model.sectionsToSave [ section ]
+                droppedOnModel =
+                    moveMovingPost model post
             in
-                ( { model
-                    | issue = issue
-                    , droppedOnPost = Just post
-                    , sectionsToSave = sectionsToSave
+                ( { droppedOnModel
+                    | droppedOnPost = Just post
                   }
                 , Cmd.none
                 )
@@ -202,71 +102,62 @@ update msg model =
             , Cmd.none
             )
 
-        SaveSectionOrder section ->
-            -- Eventually make call to server to save order.
-            -- For now, just remove section from model.sectionsToSave.
-            ( { model
-                | sectionsToSave =
-                    List.filter
-                        (\s -> s /= section)
-                        model.sectionsToSave
-              }
-            , Cmd.none
-            )
+
+moveMovingPost : Model -> Post -> Model
+moveMovingPost model beforeThis =
+    repositionPosts
+        (insertMovingPost (removeMovingPost model) beforeThis)
 
 
-slidePostsDown : List Post -> List Post
-slidePostsDown posts =
-    List.map (\p -> { p | position = p.position + 1 }) posts
+repositionPosts : Model -> Model
+repositionPosts model =
+    let
+        renumber idx post =
+            { post | position = idx }
+    in
+        { model
+            | posts = List.indexedMap renumber model.posts
+        }
 
 
-dummyPost : Post
-dummyPost =
-    { id = -1
-    , title = "dummy post"
-    , url = ""
-    , approved = False
-    , pubDate = ""
-    , submitter = -1
-    , position = -1
-    , links = []
-    }
+removeMovingPost : Model -> Model
+removeMovingPost model =
+    let
+        removeThis =
+            case model.movingPost of
+                Nothing ->
+                    Debug.crash ("stupid")
+
+                Just movingPost ->
+                    movingPost
+    in
+        { model
+            | posts = List.filter (\p -> p /= removeThis) model.posts
+        }
 
 
-getPostForMaybePost : Maybe Post -> Post
-getPostForMaybePost maybe =
-    case maybe of
-        Nothing ->
-            -- this can't be correct
-            dummyPost
-
-        Just maybe ->
-            maybe
-
-
-insertPostInSection : Section -> Post -> Post -> Section
-insertPostInSection section insertThis beforeThis =
+insertMovingPost : Model -> Post -> Model
+insertMovingPost model beforeThis =
     let
         head =
-            List.take (beforeThis.position - 1) section.posts
+            List.take (beforeThis.position - 1) model.posts
 
         tail =
-            List.drop beforeThis.position section.posts
+            List.drop (beforeThis.position - 1) model.posts
 
-        -- downslidTail =
-        --     slidePostsDown tail
+        movingPost =
+            model.movingPost
 
-        _ = Debug.log "section.posts: " section.posts
+        insertThis =
+            case model.movingPost of
+                Nothing ->
+                    Debug.crash ("stupid")
 
-
-        insertThisPositionedPost =
-            { insertThis | position = beforeThis.position }
-
-        posts =
-            head ++ [ insertThisPositionedPost ] ++ tail
+                Just movingPost ->
+                    movingPost
     in
-        { section
-            | posts = posts
+        { model
+            | posts = head ++ [ insertThis ] ++ tail
         }
 
 
@@ -276,114 +167,30 @@ insertPostInSection section insertThis beforeThis =
 
 view : Model -> Html Msg
 view model =
-    Grid.container []
-        [ CDN.stylesheet
-        , Grid.row []
-            [ Grid.col []
-                [ text model.status
-                , viewIssue model
-                ]
+    div [] (viewPosts model)
+
+
+viewPosts : Model -> List (Html Msg)
+viewPosts model =
+    let
+        sortedPosts =
+            List.sortBy .position model.posts
+    in
+        List.map viewPost sortedPosts
+
+
+viewPost : Post -> Html Msg
+viewPost post =
+    div
+        [ attribute "draggable" "true"
+        , onDragOver <| DragOver post
+        , onDragStart <| DragStart post
+        , onDragEnd <| DragEnd
+        , onDrop <| DropOn post
+        ]
+        [ h3 []
+            [ text (toString post.position)
+            , text ".)  "
+            , text post.title
             ]
         ]
-
-
-viewIssue : Model -> Html Msg
-viewIssue model =
-    Accordion.config AccordionMsg
-        |> Accordion.withAnimation
-        |> Accordion.cards (viewSections model)
-        |> Accordion.view model.accordionState
-
-
-viewSections : Model -> List (Accordion.Card Msg)
-viewSections model =
-    let
-        viewThisModelsSection =
-            viewSection model
-    in
-        List.map viewThisModelsSection model.issue.sections
-
-
-viewSection : Model -> Section -> Accordion.Card Msg
-viewSection model section =
-    let
-        showSaveButton =
-            List.member section model.sectionsToSave
-    in
-        Accordion.card
-            { id = section.name
-            , options = []
-            , header =
-                Accordion.header [] <|
-                    Accordion.toggle []
-                        [ text
-                            (section.name
-                                ++ "("
-                                ++ toString (List.length section.posts)
-                                ++ ")"
-                            )
-                        , if showSaveButton then
-                            button
-                                [ onClick (SaveSectionOrder section) ]
-                                [ text "Save" ]
-                          else
-                            text ""
-                        ]
-            , blocks =
-                [ Accordion.block [] (viewPosts model section) ]
-            }
-
-
-viewPosts : Model -> Section -> List (Block.Item Msg)
-viewPosts model section =
-    let
-        viewThisSectionsPosts =
-            viewPost model
-
-        sortedPosts =
-            List.sortBy .position section.posts
-    in
-        List.map viewThisSectionsPosts sortedPosts
-
-
-viewPost :
-    Model
-    -> Post
-    -> Block.Item Msg
-viewPost model post =
-    Block.custom
-        (Card.config
-            [ Card.attrs
-                [ attribute "draggable" "true"
-                , onDragOver <| DragOver post
-                , onDragStart <| DragStart post
-                , onDragEnd <| DragEnd
-                , onDrop <| DropOn post
-                ]
-            ]
-            |> Card.block []
-                [ Block.text [] [ text post.title ] ]
-            |> Card.view
-        )
-
-
-
--- Subscriptions
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Accordion.subscriptions model.accordionState AccordionMsg
-
-
-
--- HTTP
-
-
-getIssue : Int -> Cmd Msg
-getIssue id =
-    let
-        url =
-            "http://bulletin.aashe.org/api/issue/" ++ toString id ++ "/"
-    in
-        Http.send LoadIssue (Http.get url decodeIssue)
