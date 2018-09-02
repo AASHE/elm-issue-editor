7fa413f57deaef9ef112c179b27226cfe2b11394
Robert Erb <rerb@mbp.local>
2018-09-02 11:25:34 -0400

    Teach Post decoder/encoder how to handle optional fields

--- src/Post.elm
+++ src/Post.elm
@@ -1,210 +1,70 @@
 module Post exposing (..)
 
--- import Json.Encode
-
 import Json.Decode as JD
-import Json.Decode.Pipeline exposing (decode, required)
+import Json.Decode.Pipeline exposing (decode, optional, required)
+import Json.Encode
+import Maybe.Extra
 
 
--- @TODO - decode dates into Date.Date's, not strings
--- @TODO - encode dates Date.Date's into strings in same format API gives them
-
-type Post
-    = Event Int String String Bool String Int Int String String String String String String
-    | Job Int String String Bool String Int Int String String
-    | NewResource Int String String Bool String Int Int String String
-    | Opportunity Int String String Bool String Int Int String String
-    | Story Int String String Bool String Int Int String String String
-
-
-postDecoder : JD.Decoder Post
-postDecoder =
-    JD.field "post_type" JD.string
-        |> JD.andThen postFromPostType
-
-
-postFromPostType : String -> JD.Decoder Post
-postFromPostType postType =
-    case postType of
-        "EVENT" ->
-            eventDecoder
-
-        "JOB" ->
-            jobDecoder
-
-        "NEWRESOURCE" ->
-            newResourceDecoder
-
-        "OPPORTUNITY" ->
-            opportunityDecoder
-
-        "STORY" ->
-            storyDecoder
-
-        _ ->
-            JD.fail ("Invalid type of post: " ++ postType)
+type alias Post =
+    { id : Int
+    , title : String
+    , url : String
+    , approved : Bool
+    , pubDate : String
+    , submitter : Int
+    , position : Int
+    , postType : String
+    , startDate : Maybe String
+    , endDate : Maybe String
+    , time : Maybe String
+    , organization : Maybe String
+    , location : Maybe String
+    , organization : Maybe String
+    , blurb : Maybe String
+    , date : Maybe String
+    }
 
 
-eventDecoder : JD.Decoder Post
-eventDecoder =
-    decode Event
+decodePost : JD.Decoder Post
+decodePost =
+    decode Post
         |> required "id" JD.int
         |> required "title" JD.string
         |> required "url" JD.string
         |> required "approved" JD.bool
-        |> required "pub_date" JD.string
+        |> required "pubDate" JD.string
         |> required "submitter" JD.int
         |> required "position" JD.int
-        |> required "post_type" JD.string
-        |> required "startDate" JD.string
-        |> required "endDate" JD.string
-        |> required "time" JD.string
-        |> required "organization" JD.string
-        |> required "location" JD.string
+        |> required "postType" JD.string
+        |> optional "startDate" (JD.map Just JD.string) Nothing
+        |> optional "endDate" (JD.map Just JD.string) Nothing
+        |> optional "time" (JD.map Just JD.string) Nothing
+        |> optional "organization" (JD.map Just JD.string) Nothing
+        |> optional "location" (JD.map Just JD.string) Nothing
+        |> optional "organization" (JD.map Just JD.string) Nothing
+        |> optional "blurb" (JD.map Just JD.string) Nothing
+        |> optional "date" (JD.map Just JD.string) Nothing
 
 
-jobDecoder : JD.Decoder Post
-jobDecoder =
-    decode Job
-        |> required "id" JD.int
-        |> required "title" JD.string
-        |> required "url" JD.string
-        |> required "approved" JD.bool
-        |> required "pub_date" JD.string
-        |> required "submitter" JD.int
-        |> required "position" JD.int
-        |> required "post_type" JD.string
-        |> required "organization" JD.string
-
-
-newResourceDecoder : JD.Decoder Post
-newResourceDecoder =
-    decode NewResource
-        |> required "id" JD.int
-        |> required "title" JD.string
-        |> required "url" JD.string
-        |> required "approved" JD.bool
-        |> required "pub_date" JD.string
-        |> required "submitter" JD.int
-        |> required "position" JD.int
-        |> required "post_type" JD.string
-        |> required "blurb" JD.string
-
-
-opportunityDecoder : JD.Decoder Post
-opportunityDecoder =
-    decode Opportunity
-        |> required "id" JD.int
-        |> required "title" JD.string
-        |> required "url" JD.string
-        |> required "approved" JD.bool
-        |> required "pub_date" JD.string
-        |> required "submitter" JD.int
-        |> required "position" JD.int
-        |> required "post_type" JD.string
-        |> required "blurb" JD.string
-
-
-storyDecoder : JD.Decoder Post
-storyDecoder =
-    decode Story
-        |> required "id" JD.int
-        |> required "title" JD.string
-        |> required "url" JD.string
-        |> required "approved" JD.bool
-        |> required "pub_date" JD.string
-        |> required "submitter" JD.int
-        |> required "position" JD.int
-        |> required "post_type" JD.string
-        |> required "blurb" JD.string
-        |> required "date" JD.string
-
-
-
--- type alias Event =
---     { id : Int
---     , title : String
---     , url : String
---     , approved : Bool
---     , pubDate : String
---     , submitter : Int
---     , position : Int
---     , links : List Link
---     , startDate : String
---     , endDate : String
---     , time : String
---     , location : String
---     , organization : String
---     }
--- type alias Job =
---     { id : Int
---     , title : String
---     , url : String
---     , approved : Bool
---     , pubDate : String
---     , submitter : Int
---     , position : Int
---     , links : List Link
---     , organization : String
---     }
--- type alias Opportunity =
---     { id : Int
---     , title : String
---     , url : String
---     , approved : Bool
---     , pubDate : String
---     , submitter : Int
---     , position : Int
---     , links : List Link
---     , blurb : String
---     , date : String
---     }
--- type alias NewResource =
---     { id : Int
---     , title : String
---     , url : String
---     , approved : Bool
---     , pubDate : String
---     , submitter : Int
---     , position : Int
---     , links : List Link
---     , postType : String
---     , blurb : String
---     }
--- type alias Story =
---     { id : Int
---     , title : String
---     , url : String
---     , approved : Bool
---     , pubDate : String
---     , submitter : Int
---     , position : Int
---     , links : List Link
---     , postType : String
---     , blurb : String
---     }
--- decodePost : JD.Decoder Post
--- decodePost =
---     decode Post
---         |> required "id" JD.int
---         |> required "title" JD.string
---         |> required "url" JD.string
---         |> required "approved" JD.bool
---         |> required "pub_date" JD.string
---         |> required "submitter" JD.int
---         |> required "position" JD.int
---         |> required "links" JD.list decodeLink
---         |> required "post_type" JD.string
---         |> JD.andThen postFromPostType
--- encodePost : Model -> Json.Encode.Value
--- encodePost record =
---     Json.Encode.object
---         [ ( "id", Json.Encode.int <| record.id )
---         , ( "title", Json.Encode.string <| record.title )
---         , ( "url", Json.Encode.string <| record.url )
---         , ( "approved", Json.Encode.bool <| record.approved )
---         , ( "pubDate", Json.Encode.string <| record.pubDate )
---         , ( "submitter", Json.Encode.int <| record.submitter )
---         , ( "position", Json.Encode.int <| record.position )
---         , ( "links", Json.Encode.list <| List.map encodeLink <| record.links )
---         ]
+encodePost : Post -> Json.Encode.Value
+encodePost record =
+    [ Just ( "id", Json.Encode.int record.id )
+    , Just ( "title", Json.Encode.string record.title )
+    , Just ( "url", Json.Encode.string record.url )
+    , Just ( "approved", Json.Encode.bool record.approved )
+    , Just ( "pubDate", Json.Encode.string record.pubDate )
+    , Just ( "submitter", Json.Encode.int record.submitter )
+    , Just ( "position", Json.Encode.int record.position )
+    , Just ( "postType", Json.Encode.string record.postType )
+    , Maybe.map ((,) "startDate" << Json.Encode.string) record.startDate
+    , Maybe.map ((,) "endDate" << Json.Encode.string) record.endDate
+    , Maybe.map ((,) "time" << Json.Encode.string) record.time
+    , Maybe.map ((,) "organization" << Json.Encode.string) record.organization
+    , Maybe.map ((,) "location" << Json.Encode.string) record.location
+    , Maybe.map ((,) "organization" << Json.Encode.string) record.organization
+    , Maybe.map ((,) "blurb" << Json.Encode.string) record.blurb
+    , Maybe.map ((,) "date" << Json.Encode.string) record.date
+    ]
+        |> Maybe.Extra.values
+        |> Json.Encode.object
